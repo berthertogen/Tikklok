f4c33cf4642610db68f8b8db1aa8056283cc1505
Bert Hertogen <bert.hertogen@live.be>
2018-04-16 21:09:34 +0200

    Enable DB

--- Tikklok/Data/TiklineDb.cs
+++ Tikklok/Data/TiklineDb.cs
@@ -1,4 +1,4 @@
-﻿//using LiteDB;
+﻿using LiteDB;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,35 +12,34 @@ namespace Tikklok.Data
 
         public IEnumerable<Tikline> Get(string userid)
         {
-            return new List<Tikline>();
-            //using (var db = new LiteDatabase(DBFILENAME))
-            //{
-            //    // Get a collection (or create, if doesn't exist)
-            //    var tiklines = db.GetCollection<TiklineDTO>("tiklines");
-            //    return tiklines
-            //        .Find(tl => tl.UserId == userid)
-            //        .Select(tl => new Tikline
-            //        {
-            //            UserId = tl.UserId,
-            //            Time = tl.Time,
-            //            Action = tl.Action
-            //        });
-            //}
+            using (var db = new LiteDatabase(DBFILENAME))
+            {
+                // Get a collection (or create, if doesn't exist)
+                var tiklines = db.GetCollection<TiklineDTO>("tiklines");
+                return tiklines
+                    .Find(tl => tl.UserId == userid)
+                    .Select(tl => new Tikline
+                    {
+                        UserId = tl.UserId,
+                        Time = tl.Time,
+                        Action = tl.Action
+                    });
+            }
         }
 
         public void Insert(Tikline tikline)
         {
-            //using (var db = new LiteDatabase(DBFILENAME))
-            //{
-            //    var tiklines = db.GetCollection<TiklineDTO>("tiklines");
-            //    tiklines.Insert(new TiklineDTO
-            //    {
-            //        Id = Guid.NewGuid(),
-            //        UserId = tikline.UserId,
-            //        Time = tikline.Time,
-            //        Action = tikline.Action,
-            //    });
-            //}
+            using (var db = new LiteDatabase(DBFILENAME))
+            {
+                var tiklines = db.GetCollection<TiklineDTO>("tiklines");
+                tiklines.Insert(new TiklineDTO
+                {
+                    Id = Guid.NewGuid(),
+                    UserId = tikline.UserId,
+                    Time = tikline.Time,
+                    Action = tikline.Action,
+                });
+            }
         }
     }
 }
